719c43c6fb9d8c0bbb5f6b9aca70727dd7876ad3
leandro passos <161339896+leandrwo@users.noreply.github.com>
2024-06-17 12:17:58 -0300

    Add files via upload

---
+++ jogo1-polo-main/js/main.js
@@ -0,0 +1,46 @@
+
+const blob = document.querySelector('.blob');
+const pipe = document.querySelector('.pipe');
+
+
+const jump = () => {
+    blob.classList.add('jump');
+
+    setTimeout(() => {
+        blob.classList.remove('jump');
+
+    }, 500 );
+
+}
+const loop = setInterval(()=>{
+    const pipePosition = pipe.offsetLeft;
+    const blobPosition = +window.getComputedStyle(blob).bottom.replace('px', '');
+
+    if (pipePosition <= 120 && pipePosition >0 && blobPosition<80) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        blob.style.animation = 'none';
+        blob.style.bottom = `${pipePosition}px`;
+
+        blob.src = './img/game-over.png';
+        blob.style.width = '500px';
+        blob.style.marginLeft = '500px' ;
+
+        clearInterval(loop);
+    }
+
+
+    score++;
+    scoreText.t = "Pontuação: " + score;
+    scoreText.draw();
+
+    if (score > highscore) {
+        highscore = score;
+        highscoreText.t = "Highscore: " + highscore;
+    }
+
+    highscoreText.draw();
+
+}, 10) 
+document.addEventListener('keydown', jump);
